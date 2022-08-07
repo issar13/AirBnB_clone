a56c54cfba1f7ba83d24dfaeee2023f10cef7471
issar13 <issa_suleiman008@hotmail.com>
2022-08-07 17:58:20 +0300

    pain

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,12 +14,12 @@ class FileStorage:
 
     def all(self):
         """ Returns the dictionary __objects """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name >.id """
         key = obj.__class__.__name__ + "." + obj.id
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """ Serializes __objects to the JSON file """
@@ -43,8 +43,10 @@ class FileStorage:
         dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
                'City': City, 'Amenity': Amenity, 'State': State,
                'Review': Review}
-
-        if os.path.exists(FileStorage.__file_path) is True:
-            with open(FileStorage.__file_path, 'r') as f:
-                for key, value in json.load(f).items():
-                    self.new(dct[value['__class__']](**value))
+        try:
+            with open(self.__file_path, 'r', encoding='UTF-8') as file:
+                jn = json.load(file)
+            for key in jn:
+                self.__objects[key] = dct[jn[key]["__class__"]](**jn[key])
+        except FileNotFoundError:
+            pass
